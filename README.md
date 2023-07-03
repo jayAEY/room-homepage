bcc7c823c6fd3ad3924392e7ba45608c1e8f0453
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-07-03 14:21:07 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# room-homepage
-landing page with a precise layout
+# Frontend Mentor - Room homepage solution
+
+This is a solution to the [Room homepage challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![](./images/Screen%20Shot%202023-07-03%20at%2013.30.18.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+This project was great for practicing layout and positioning. It took a while for me to get the arrows in the desktop version to stay aligned with the image in the about section without disrupting the whole layout.
+
+It was also nice to figure out how to control the slideshow with keyboard presses. I usually dont use keyboard events with normal websites, so it was nice to have a reason to practice it again.
+
+## Author
+
+- Frontend Mentor - [@jayAEY](https://www.frontendmentor.io/profile/jayAEY)
---
+++ script.js
@@ -0,0 +1,51 @@
+let mobileHeroImages = document.querySelectorAll(".mobile-hero-image")
+let desktopHeroImages = document.querySelectorAll(".desktop-hero-image")
+let heroText = document.querySelectorAll(".hero-text")
+let nav = document.querySelector("nav")
+
+// count fot slideshow
+let heroCount = 0
+
+// handle mobile menu
+function menu(element) {
+    element.id === "menu-icon" ? nav.style.display = "grid" : nav.style.display = "none"
+}
+
+function changeSlide(arrow) {
+    //change image if right arrow is clicked or pressed on keyboard
+    if (arrow.id == "slideshow-right-arrow" || arrow.key === "ArrowRight") {
+        mobileHeroImages[heroCount].style.display = "none"
+        desktopHeroImages[heroCount].style.display = "none"
+        heroText[heroCount].style.display = "none"
+        heroCount === 2 ? heroCount = 0 : heroCount++
+        mobileHeroImages[heroCount].style.display = "flex"
+        desktopHeroImages[heroCount].style.display = "flex"
+        heroText[heroCount].style.display = "flex"
+
+    }
+
+    //change image if left arrow is clicked or pressed on keyboard
+    if (arrow.id === "slideshow-left-arrow" || arrow.key === "ArrowLeft") {
+        mobileHeroImages[heroCount].style.display = "none"
+        desktopHeroImages[heroCount].style.display = "none"
+        heroText[heroCount].style.display = "none"
+        heroCount === 0 ? heroCount = 2 : heroCount--
+        mobileHeroImages[heroCount].style.display = "flex"
+        desktopHeroImages[heroCount].style.display = "flex"
+        heroText[heroCount].style.display = "flex"
+    }
+}
+
+//display desktop menu if window is wider than 700px
+function desktopNav() {
+    if (window.innerWidth >= 700) {
+        nav.style.display = "grid"
+    } else {
+        nav.style.display = "none"
+    }
+}
+
+// check for window size
+window.addEventListener("resize", () => desktopNav())
+// check for keyboard presses
+document.addEventListener("keyup", (e) => changeSlide(e))
